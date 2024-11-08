7604dbd1ed92f4c86629d834759232725d77b782
Buchi Anyaele <121482145+banyaele22@users.noreply.github.com>
2024-11-08 01:15:01 -0600

    tweaked subscribe page

---
+++ console.js
@@ -0,0 +1,9 @@
+const name = 'dev ed';
+
+function logger(){
+    console.log("party tIme");
+    console.log("party tIme");
+    console.log("party tIme");
+    console.log("party tIme");
+}
+logger();
